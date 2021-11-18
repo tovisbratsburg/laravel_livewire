88ea27b9b5a32783f79dc769846fc98eeafbc64e
Tovis Bratsburg <tovisbratsburg@gmail.com>
2021-11-18 10:32:24 -0500

    Tailwind

--- resources/views/counter.blade.php
+++ resources/views/counter.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Livewire</title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/d00f84958a.js" crossorigin="anonymous"></script>
     @livewireStyles
 </head>
