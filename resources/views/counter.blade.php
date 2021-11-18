ad805fac3c99840caa745e5b95e315f5718131c3
Tovis Bratsburg <tovisbratsburg@gmail.com>
2021-11-18 10:20:59 -0500

    Counter

---
+++ resources/views/counter.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Livewire</title>
+    <script src="https://kit.fontawesome.com/d00f84958a.js" crossorigin="anonymous"></script>
+    @livewireStyles
+</head>
+
+<body>
+@livewire('counter')
+@livewireScripts
+</body>
+
+</html>
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div style="text-align: center">
+    <i wire:click="increment" class="fas fa-plus-circle fa-lg"></i>
+    <h1>{{$count}}</h1>
+    <i wire:click="decrement" class="fas fa-minus-circle fa-lg"></i>
+</div>
