eb61a3fddc635f98f4e2ddf8865899d9bb43fa57
Tovis Bratsburg <tovisbratsburg@gmail.com>
2021-11-18 10:38:43 -0500

    Style Counter

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,5 +1,5 @@
-<div style="text-align: center">
-    <i wire:click="increment" class="fas fa-plus-circle fa-lg"></i>
-    <h1>{{$count}}</h1>
-    <i wire:click="decrement" class="fas fa-minus-circle fa-lg"></i>
+<div class="text-center mt-8">
+    <i wire:click="increment" class="fas fa-plus-circle fa-2x text-blue-500 text-2xl"></i>
+    <h1 class="text-4xl text-green-700">{{$count}}</h1>
+    <i wire:click="decrement" class="fas fa-minus-circle fa-2x text-blue-500 text-2xl"></i>
 </div>
